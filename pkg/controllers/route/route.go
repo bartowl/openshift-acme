0ae25dc0d9a6935b956e6d403fc406cc537f434e
Sowa, Bartlomiej <Bartlomiej.Sowa@opitz-consulting.com>
2019-10-14 14:26:43 +0200

    try1: fix long URLs with env Variable

--- pkg/controllers/route/route.go
+++ pkg/controllers/route/route.go
@@ -513,10 +513,15 @@ func (rc *RouteController) handle(key string) error {
 
 		case acme.StatusValid:
 			glog.V(4).Infof("Authorization %q for Route %s successfully validated", authorization.URI, key)
+			// workaround for domains longer than 63 characters
+			cn := routeReadOnly.Spec.Host
+			if len(cn)>63 {
+				cn=os.Getenv("CN_FOR_LONG_DOMAINS")
+			}
 			// provision cert
 			template := x509.CertificateRequest{
 				Subject: pkix.Name{
-// remove CN: obsolete					CommonName: routeReadOnly.Spec.Host,
+					CommonName: cn,
 				},
 			}
 			template.DNSNames = append(template.DNSNames, routeReadOnly.Spec.Host)
